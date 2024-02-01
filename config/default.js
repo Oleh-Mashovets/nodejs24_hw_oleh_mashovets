f43cee619beb95ccc03728d0f83cc33671b49d36
Oleh-Mashovets <mashovets700@gmail.com>
2024-02-01 16:46:30 +0200

    fs

--- config/default.js
+++ config/default.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 module.exports = {
     LOG_LEVEL: process.env.LOG_LEVEL || 'warn',
     COLORS_ENABLED: process.env.COLORS_ENABLED || '0',
--- main.js
+++ main.js
@@ -1,14 +1,23 @@
-require('dotenv').config();
 const config = require('config');
+
+const fs = require('fs');
+
+const fileSync = require('./file_sync');
+fileSync.start();
 
 const COLORS_ENABLED = config.get('COLORS_ENABLED');
 const LOG_LEVEL = config.get('LOG_LEVEL');
 
-console.log('COLORS_ENABLED:', COLORS_ENABLED);
-console.log('LOG_LEVEL:', LOG_LEVEL);
-
 const logger = require('./utils/logger')('main', COLORS_ENABLED, LOG_LEVEL);
 
-logger.info('the script is running!');
-logger.warn('the script is running with warning!');
-logger.error('the script is running with error!');
+// logger.info('the script is running!');
+// logger.warn('the script is running with warning!');
+// logger.error('the script is running with error!');
+
+
+fs.writeFileSync('./source/file_1', "First string");
+fs.writeFileSync('./source/file_2', "First string");
+fs.writeFileSync('./target/file_2', "First string");
+
+fs.mkdirSync('./source/dir_1', {recursive: true});
+fs.writeFileSync('./source/dir_1/file_3', "First string");
